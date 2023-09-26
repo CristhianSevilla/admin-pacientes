da8cacbd0473a5fc5a10fe49aecc8af0b4c93765
Cristhian Sevilla <crissev17@gmail.com>
2023-09-25 18:28:19 -0600

    Leer datos de un formulario con ref y v-model

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,11 +1,16 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const nombre = ref("");
+</script>
 <template>
   <div class="md:w-1/2 xl:px-10">
-    <h2 class="text-2xl mt-5 text-center mb-10">
+    <h2 class="text-2xl mt-5 text-center mb-10 font-bold md:font-normal">
       Agrega Pacientes y
-      <span class="text-indigo-700 font-bold">Adminístralos</span>
+      <span class="text-indigo-600 font-bold">Adminístralos</span>
     </h2>
 
+    {{ nombre }}
     <form
       class="bg-indigo-100 bg-opacity-60 shadow-md rounded-lg py-10 px-5 mb-10"
     >
@@ -18,6 +23,7 @@
           type="text"
           class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
           placeholder="Nombre del paciente"
+          v-model="nombre"
         />
       </div>
       <div class="mb-5">
@@ -65,7 +71,7 @@
 
       <input
         type="submit"
-        class="bg-indigo-600 w-full p-3 text-white uppercase font-bold"
+        class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-800 hover:cursor-pointer"
         value="Registrar Paciente"
       />
     </form>
