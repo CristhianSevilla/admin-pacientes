adedd93db1fc2a9de6f1a784a452c601d6ed0d82
Cristhian Sevilla <crissev17@gmail.com>
2023-09-26 13:58:04 -0600

    Agregando dinamismo a la alerta con  un computed properties

--- src/components/Alerta.vue
+++ src/components/Alerta.vue
@@ -1,15 +1,22 @@
 <script setup>
+import { computed } from "vue";
+
 const props = defineProps({
   alerta: {
     type: Object,
     required: true,
   },
 });
+
+const isError = computed(() => {
+  return props.alerta.tipo === "error";
+});
 </script>
 
 <template>
   <div
-    class="text-white uppercase p-3 bg-red-500 rounded-md text-center font-bold mb-2"
+    :class="[isError ? 'bg-red-500' : 'bg-green-500']"
+    class="text-white uppercase p-3 rounded-md text-center font-bold mb-2"
   >
     {{ alerta.mensaje }}
   </div>
